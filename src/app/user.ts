0c9b466031d9c0b9bf6b82d35a96b05a8f49c8ca
odetteahishakiye <ahiode6@gmail.com>
2019-08-29 14:51:52 +0200

    Add search button

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,6 @@
 export class User {
+    public repoList:boolean;
+    constructor(public username: string, public profile: string, public repoNUmber:number ){
+        this.repoList=false;
+    }
 }
