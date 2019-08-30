10d510e7f9d6c21b90f3f02a691306e5f92a5a1d
odetteahishakiye <ahiode6@gmail.com>
2019-08-30 15:35:28 +0200

    Add a repoList service

--- src/app/github-request.service.ts
+++ src/app/github-request.service.ts
@@ -11,22 +11,29 @@ import { User } from '../app/user';
 export class GithubRequestService {
   user: User;
   constructor(private http: HttpClient) {
-    this.user = new User("", "", "", 0,0,new Date(),0,"");
+    this.user = new User("", "", "", 0,0,0,"");
   }
 
   githubRequest() {
     interface ApiResponse {
+      
       login: string;
       name: string;
       avatar_url: string;
+      followers:number;
+      following:number;
       public_repos: number;
+      url: string;
     }
     let promise = new Promise((resolve, reject) => {
       this.http.get<ApiResponse>("https://api.github.com/users/ahiodette?access_token="+environment.key).toPromise().then(response => {
         this.user.login = response.login
         this.user.name = response.name
         this.user.avatar_url= response.avatar_url
+        this.user.followers=response.followers
+        this.user.following=response.following
         this.user.public_repos = response.public_repos
+        this.user.url=response.url
 
         resolve()
       },
@@ -34,7 +41,10 @@ export class GithubRequestService {
           this.user.login = "User not found"
           this.user.name = "No name is found"
           this.user.avatar_url = "No profile picture"
+          this.user.followers=0
+          this.user.following=0
           this.user.public_repos = 0
+          this.user.url= "Not found"
 
           reject(error)
         })
--- src/app/user.ts
+++ src/app/user.ts
@@ -6,9 +6,9 @@ export class User {
         public avatar_url: string, 
         public followers:number,
         public following:number,
-        public created_at:Date,
+        // public created_at:Date,
         public public_repos:number,
-        public repos_url:string
+        public url:string
          ){
         this.repoList=false;
     }
