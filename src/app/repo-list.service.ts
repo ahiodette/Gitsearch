9ad28de97cbf7b80d70f1c6f2aa7b236ce1d585e
odetteahishakiye <ahiode6@gmail.com>
2019-08-30 17:05:56 +0200

    Change output structure

--- src/app/repo-list.service.ts
+++ src/app/repo-list.service.ts
@@ -1,41 +1,41 @@
-import { Injectable } from '@angular/core';
-import { environment } from '../environments/environment';
-import { HttpClient } from '@angular/common/http';
-import { Repository } from '../app/repository';
-import { NullAstVisitor } from '@angular/compiler';
+// import { Injectable } from '@angular/core';
+// import { environment } from '../environments/environment';
+// import { HttpClient } from '@angular/common/http';
+// import { Repository } from '../app/repository';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class RepoListService {
 
-  repository: Repository;
-  constructor(private http: HttpClient) {
-    this.repository = new Repository("", "", new Date());
-  }
-  repoListService() {
-    interface Apiresponse {
-      name: string;
-      html_url: string;
-      created_at: Date;
-    }
-    let promise = new Promise((resolve, reject) => {
-      this.http.get<Apiresponse>("https://api.github.com/users/ahiodette/repos?access_token=" + environment.key).toPromise().then(response => {
-        this.repository.name = response.name
-        this.repository.html_url = response.html_url
-        this.repository.created_at = response.created_at
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class RepoListService {
 
-        resolve()
-      },
+//   repository: Repository;
+//   constructor(private http: HttpClient) {
+//     this.repository = new Repository("", "", new Date());
+//   }
+//   repoListService() {
+//     interface Apiresponse {
+//       name: string;
+//       html_url: string;
+//       created_at: Date;
+//     }
+//     let promise = new Promise((resolve, reject) => {
+//       this.http.get<Apiresponse>("https://api.github.com/users/ahiodette/repos?access_token=" + environment.key).toPromise().then(response => {
+//         this.repository.name = response.name
+//         this.repository.url = response.url
+//         this.repository.created_at = response.created_at
 
-        error =>{
-          this.repository.name="RepoName not found"
-          this.repository.html_url="No repo URL found"
-          this.repository.created_at=new Date
+//         resolve()
+//       },
 
-          reject(error)
-        })
-    })
-    return promise
-  }
-}
+//         error =>{
+//           this.repository.name="RepoName not found"
+//           this.repository.html_url="No repo URL found"
+//           this.repository.created_at=new Date
+
+//           reject(error)
+//         })
+//     })
+//     return promise
+//   }
+// }
