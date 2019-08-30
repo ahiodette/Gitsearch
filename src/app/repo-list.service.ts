60f1b22771a7531df319fc2e05b8293d1aa68e49
odetteahishakiye <ahiode6@gmail.com>
2019-08-30 16:25:17 +0200

    Define repoList service

--- src/app/repo-list.service.ts
+++ src/app/repo-list.service.ts
@@ -1,12 +1,41 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { Repository }
+import { Repository } from '../app/repository';
+import { NullAstVisitor } from '@angular/compiler';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RepoListService {
 
-  constructor() { }
+  repository: Repository;
+  constructor(private http: HttpClient) {
+    this.repository = new Repository("", "", new Date());
+  }
+  repoListService() {
+    interface Apiresponse {
+      name: string;
+      html_url: string;
+      created_at: Date;
+    }
+    let promise = new Promise((resolve, reject) => {
+      this.http.get<Apiresponse>("https://api.github.com/users/ahiodette/repos?access_token=" + environment.key).toPromise().then(response => {
+        this.repository.name = response.name
+        this.repository.html_url = response.html_url
+        this.repository.created_at = response.created_at
+
+        resolve()
+      },
+
+        error =>{
+          this.repository.name="RepoName not found"
+          this.repository.html_url="No repo URL found"
+          this.repository.created_at=new Date
+
+          reject(error)
+        })
+    })
+    return promise
+  }
 }
